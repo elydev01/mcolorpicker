8e467c049a9b0ddfdbd8c24e5827a57192380861
elydev01 <91912825+elydev01@users.noreply.github.com>
2021-10-12 20:49:11 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
   name = 'mcolorpicker',
   packages = ['mcolorpicker'],
-  version = '0.1',
+  version = '0.1.1',
   license='MIT',
   description = 'A simple, minimalist and beautiful color picker for kivy based applications. This color picker is easy to use and integrate into your application to allow your application users to choose the color in a simple way.',
   author = 'KOUA ELYSEE',
   author_email = 'elydev01@gmail.com',
   url = 'https://github.com/elydev01/mcolorpicker',
   download_url = 'https://github.com/elydev01/mcolorpicker/archive/refs/tags/1.0.tar.gz',
-  keywords = ['kivy', 'color', 'picker', 'kivymd', 'choose', 'mobile'],
+  keywords = ['kivy', 'color', 'picker', 'colorpicker', 'kivymd', 'choose', 'mobile'],
   install_requires=[
           'kivy',
           'kivymd',
@@ -24,7 +24,7 @@ setup(
 
     # Indiquez à qui votre projet est destiné
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools :: Mobile Development',
+    'Topic :: Software Development :: Widget Sets',
 
     # Choisissez votre licence comme vous le souhaitez
     'License :: OSI Approved :: MIT License',
